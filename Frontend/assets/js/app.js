6da09464e55edf311030e1dfc55ae61e52f7eabe
Martinolotto <martinotiktok1@gmail.com>
2026-06-10 20:06:12 -0300

    chore: definir estructura inicial de carpetas y arquitectura del proyecto

---
+++ Frontend/assets/js/app.js
@@ -0,0 +1,14 @@
+function sumar (a, b, callback){
+    const resultado = a+b;
+    callback (resultado)
+}
+//
+console.log('inicio')
+//
+function obtenerdatos (callback){
+    console.log('buscando');
+}
+setTimeout(function){
+    const usuario = { id: 1, nombre: 'Carlos'}
+
+}
