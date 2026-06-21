203cf9b8530070e7d55ea7a19ff826ab1284256c
gomez ivan <ivanemmanuelgomez2017@gmail.com>
2026-06-21 09:49:33 -0300

    feat: se creo una autenticacion en la base d datos

--- Backend/server.js
+++ Backend/server.js
@@ -3,12 +3,23 @@
 //debe recibir los datos de login.html, mediante POST
 
 import express from 'express';
+import sequelize from './src/config/database.js';
 const server = express();
-
 
 const PORT = 3000;
 server.use(express.json())
 
+const conexionBD = async ()=>{
+    try {
+        const verificacion = await sequelize.authenticate();
+        console.log(verificacion);
+        console.log('Conexión a la BD exitosa');
+    } catch (error) {
+        console.log('Error:', error.message);
+    }
+}
+
+conexionBD()
 
 server.listen(PORT, ()=>{
     console.log(`server prendido en elpuerto ${PORT}`);
