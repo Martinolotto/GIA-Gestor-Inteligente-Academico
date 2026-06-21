47162111d6be5c4725c269700407e11014a8e16a
gomez ivan <ivanemmanuelgomez2017@gmail.com>
2026-06-21 08:50:32 -0300

    feat: se incializron las configuraciones basicas del servidor y se corriegieron cosas como el require que se elimino completamente

--- Backend/server.js
+++ Backend/server.js
@@ -2,11 +2,14 @@
 // levantar el servidor express en un puerto y configurar
 //debe recibir los datos de login.html, mediante POST
 
-//al recibir los datos los guarda en un array y debe responder
-//si el registro es exitoso, entonces redirige a la institucion a panel-control.html, donde se le habilita el menu expandible para que completen su perfil
+import express from 'express';
+const server = express();
 
-//importante, si solo vamos a simular la base de datos con el array con objetos o si vamos a usarla para testear y despues en un futuro vincular todo,
-// aca es donde se debe importar datos.js  con un:
-require("./database/datos.js");
-// si no se importa el servidor no va a saber donde guardar los datos nuevos
-// Crear una ruta, para que el html pida las instituciones por acá.
+
+const PORT = 3000;
+server.use(express.json())
+
+
+server.listen(PORT, ()=>{
+    console.log(`server prendido en elpuerto ${PORT}`);
+})
